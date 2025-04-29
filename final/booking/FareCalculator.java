c47c8bbd691697db0017ee1c1ef4b88eac460da7
Omvishnu746 <vishnuvishnoi2004@gmail.com>
2025-04-29 22:42:56 +0530

    FareCalculator.java

--- final/booking/FareCalculator.java
+++ final/booking/FareCalculator.java
@@ -4,12 +4,14 @@ public class FareCalculator
 {
     	static int COST_PER_KM = 12;
     	static int AVG_SPEED = 40;
-
+	
+	// Calculates fare based on distance
     	public static int calculateFare(int dist) 
 	{
         	return dist * COST_PER_KM;
     	}
 
+	// Calculates ETA in minutes
     	public static int calculateETA(int dist) 
 	{
         	return (int)(((double) dist / AVG_SPEED) * 60);
